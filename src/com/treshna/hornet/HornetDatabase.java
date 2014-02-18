e969ee277473540c6fcb2e288e09535af8b21a34
Callum Trayner <callum@treshna.com>
2014-02-19 09:57:31 +1300

    minor changes to version numbers, getting ready for release.

--- src/com/treshna/hornet/HornetDatabase.java
+++ src/com/treshna/hornet/HornetDatabase.java
@@ -43,7 +43,7 @@ import com.treshna.hornet.ContentDescriptor.Visitor;
 public class HornetDatabase extends SQLiteOpenHelper {
 	
 	 public static final String DATABASE_NAME="hornet.db";
-	 private static final int DATABASE_VERSION = 93;
+	 private static final int DATABASE_VERSION = 94;
 	 //private Context theContext;
 	 //^^may be required for toasts etc at some point.
 	 
